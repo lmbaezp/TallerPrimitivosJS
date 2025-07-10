74b45007d400cb60fae426e2fedb9189f741327d
LMBP <juenpame@gmail.com>
2025-07-10 16:17:50 -0500

    Primer ejercicio

---
+++ parte1.js
@@ -0,0 +1,20 @@
+// 1. Variables y Tipos de Datos
+// Declarar variables y usar tipos de datos primitivo 
+// (number, string, boolean, null, undefined).
+
+// Ejercicio 1:
+// •Crea una variable nombre con tu nombre.
+let nombre = 'Laura';
+// •Crea una variable edad con tu edad.
+let edad = 34;
+// •Crea una variable esEstudiante con true o false.
+let esEstudiante = true;
+
+// Ejercicio 2:
+// •Declara una variable sinValor sin asignarle nada (queda undefined).
+let sinValor;
+// •Declara una variable valorNulo y asígnale null.
+let valorNulo = null;
+// •Muestra ambas en consola y observa la diferencia.
+console.log('Sin valor: ', sinValor);
+console.log('Valor nulo: ', valorNulo);

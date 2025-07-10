b9bc09ed5442c77ecb13d3a9a9e73452d78b3e24
LMBP <juenpame@gmail.com>
2025-07-10 16:27:27 -0500

    Segundo ejercicio

---
+++ parte2.js
@@ -0,0 +1,21 @@
+// 2. Ingreso de Datos por Teclado
+// Usar prompt() para pedir datos al usuario.
+const prompt = require("prompt-sync")();
+
+// Ejercicio 1:
+// •Pide al usuario su año de nacimiento con prompt().
+let anioNacimiento = parseInt(prompt('Ingresa tu año de nacimiento: '));
+// •Calcula su edad.
+let edad = 2025 - anioNacimiento;
+// •Muestra.
+console.log('Tu edad es ', edad, 'años')
+
+// Ejercicio 2:
+// •Pide dos números con prompt().
+let num1 = prompt('Ingresa un primer número: ')
+let num2 = prompt('Ingresa un segundo número: ')
+// •Convierte los datos a número.
+num1 = parseFloat(num1)
+num2 = parseFloat(num2)
+// •Muestra la suma en consola.
+console.log(num1, ' + ', num2, ' = ', num1+num2)
